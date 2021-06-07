36bb8595fd4a974a700b277c83c7004d9050befa
hp <ekowibowo030997@gmail.com>
2021-06-07 13:04:30 +0700

    pertemuan 1

---
+++ src/LatihanVariabel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class LatihanVariabel {
+    public static void main(String[] args) {
+        int JariJari;
+        double Keliling;
+        final double pi = 3.14;
+        
+        JariJari = 30;
+        Keliling = 2*pi*JariJari;
+        System.out.println("Keliling Lingkaran:"+ Keliling);
+        System.out.println("=====================");
+        
+    }
+    
+}
